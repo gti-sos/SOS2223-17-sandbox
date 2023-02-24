1a09dfc904d2b38a6c498af2b9d17f919d398324
josgaroro1 <josgaroro1@alum.us.es>
2023-02-24 12:24:53 +0100

    teoria L05

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var cool = require("cool-ascii-faces");
+
+var app = express();
+var port = 12345;
+
+app.get("/faces", (request,response)=>{ //response, la resp que queiro dar a una petición que llega
+    response.send(cool());//esto es lo que mando al navegador
+    console.log("New request")
+});
+
+app.listen(port, ()=>{
+    console.log(`Server ready in port $(port)`);
+});//funcion asincrona, con el 2º param me aseguro escucha cuando el servidor esta preparado
+
