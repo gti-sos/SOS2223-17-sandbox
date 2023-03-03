af0e6bb2c9df26d4c5b4810667d489162e59c83b
josgaroro1 <josgaroro1@alum.us.es>
2023-03-03 12:14:03 +0100

    Index y app.yaml

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express");
 var cool = require("cool-ascii-faces");
 
 var app = express();
-var port = 12345;
+var port = process.env.PORT || 12345;
 
 app.get("/faces", (request,response)=>{ //response, la resp que queiro dar a una petición que llega
     response.send(cool());//esto es lo que mando al navegador
@@ -13,28 +13,3 @@ app.get("/faces", (request,response)=>{ //response, la resp que queiro dar a una
 app.listen(port, ()=>{
     console.log(`Server ready in port ${port}`);
 });//funcion asincrona, con el 2º param me aseguro escucha cuando el servidor esta preparado
-
-//var algo = console.log("Hello world !");
-
-function log(msg){
-    console.log(msg);
-}
-
-log("Mi fantastico log");
-
-var numbers = [1,2,3,4,5]
-
-// for(var i = 0; i<numbers.length; i++){
-//     log(numbers[i]);
-// }
-
-var i = numbers
-            .filter((n)=>{
-            return n>2;
-            }).map((n)=>{
-                return n+1;
-            }).reduce((a,n)=>{
-            return a+n;
-});
-
-log(i);
